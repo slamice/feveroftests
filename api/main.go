6b8b11cf6190711f9f9984beac58d9602cec032d
issam.zeibak <issam.zeibak@gengo.com>
2015-03-13 13:49:17 +0900

    added basic rethinkdb stuffs

--- api/main.go
+++ api/main.go
@@ -2,26 +2,81 @@ package main
 
 import (
 	"encoding/json"
+	r "github.com/christopherhesse/rethinkgo"
 	"log"
 	"net/http"
 	"os"
-    "github.com/op/go-logging"
 )
 
-var sessionArray []*t.Session
+var sessionArray []*r.Session
 
-type post struct {
+type Bookmark struct {
 	Title string
-	body  []byte
+	Url   string
 }
 
-func initDb()
-{
+func initDb() {
+	session, err := r.Connect(os.Getenv("WERCKER_RETHINKDB_URL"), "gettingstarted")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = r.DbCreate("gettingstarted").Run(session).Exec()
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = r.TableCreate("bookmarks").Run(session).Exec()
+	if err != nil {
+		log.Println(err)
+	}
+
+	sessionArray = append(sessionArray, session)
 }
 
-func main()
-{
-    initDb()
-    http.HandleFunc("/", handleIndex)
-    http.HandleFunc("/user/add", handleNewUser)
+func main() {
+
+	initDb()
+
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/new", insertBookmark)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Fatal("Error: %v", err)
+	}
+}
+
+func insertBookmark(res http.ResponseWriter, req *http.Request) {
+	session := sessionArray[0]
+
+	b := new(Bookmark)
+	json.NewDecoder(req.Body).Decode(b)
+
+	var response r.WriteResponse
+
+	err := r.Table("bookmarks").Insert(b).Run(session).One(&response)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	data, _ := json.Marshal("{'bookmark':'saved'}")
+	res.Header().Set("Content-Type", "application/json; charset=utf-8")
+	res.Write(data)
+}
+
+func handleIndex(res http.ResponseWriter, req *http.Request) {
+	session := sessionArray[0]
+	var response []Bookmark
+
+	err := r.Table("bookmarks").Run(session).All(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, _ := json.Marshal(response)
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(data)
 }
---
+++ api/main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func init() {
+	initDb()
+}
+
+func TestHandleIndexReturnsWithStatusOK(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+
+	handleIndex(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Response body did not contain expected %v:\n\tbody: %v", "200", response.Code)
+	}
+}
+
+func TestHandInsertBookmarkWithStatusOK(t *testing.T) {
+	bookmark := Bookmark{"wercker", "http://wercker.com"}
+
+	b, err := json.Marshal(bookmark)
+	if err != nil {
+		t.Fatalf("Unable to marshal Bookmark")
+	}
+
+	request, _ := http.NewRequest("POST", "/new", bytes.NewReader(b))
+	response := httptest.NewRecorder()
+
+	insertBookmark(response, request)
+
+	body := response.Body.String()
+	if !strings.Contains(body, "{'bookmark':'saved'}") {
+		t.Fatalf("Response body did not contain expected %v:\n\tbody: %v", "San Francisco", body)
+	}
+}
+
+func TestHandleIndexReturnsJSON(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+
+	handleIndex(response, request)
+
+	ct := response.HeaderMap["Content-Type"][0]
+	if !strings.EqualFold(ct, "application/json") {
+		t.Fatalf("Content-Type does not equal 'application/json'")
+	}
+}
